23ae035e1e7faedaa54b6ba3424b7382375c820a
dev <mohammad.ali.asheghi@gmail.com>
2024-09-01 18:55:48 +0330

    import vazir font and config

--- contact-manager-app/src/App.jsx
+++ contact-manager-app/src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 const App = () => {
     return (
         <>
+            <p>وب اپلیکیشن مدیریت مخاطبین</p>
             <span className='fa fa-user'/>
-            <button className='btn btn-primary'>click</button>
+            <button className='btn btn-primary'>انتخاب</button>
         </>
     )
 }
