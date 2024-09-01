368761844ebe6a74d0bdc1b2025bf9e97bc43d44
dev <mohammad.ali.asheghi@gmail.com>
2024-09-01 14:32:02 +0330

    add inline style for counter

--- frontend/src/Counter.jsx
+++ frontend/src/Counter.jsx
@@ -1,12 +1,26 @@
 import PropTypes from "prop-types";
 /* eslint-disable react/prop-types */
 const Counter = ({inc, dec, res, count}) => {
+
+    const changeCountColor = () => {
+        if (count === 0)
+            return 'yellow';
+        else if (count > 0)
+            return 'green'
+        else if (count < 0)
+            return 'red';
+        else return 'aqua';
+    }
+
     return (
         <div>
-            <h1>{count}</h1>
+            <h1 style={{color: changeCountColor()}}>{count}</h1>
 
             <button onClick={inc}>+</button>
-            <button onClick={dec}>-</button>
+            <button onClick={dec} disabled={count === -5}
+                    style={{backgroundColor: count === -5 ? 'aqua' : '#1a1a1a'}}>
+                -
+            </button>
             <br/>
             <button onClick={res}>o</button>
         </div>
