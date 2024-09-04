638cbd28ff093a9f6af1c0cee957320ce38d491d
dev <mohammad.ali.asheghi@gmail.com>
2024-09-04 12:11:34 +0330

    1-add apache kafka to project 2-implement kafka producer

---
+++ oauth/src/main/java/com/mmad/oauth/util/MessageProducer.java
@@ -0,0 +1,20 @@
+package com.mmad.oauth.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public void setKafkaTemplate(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
