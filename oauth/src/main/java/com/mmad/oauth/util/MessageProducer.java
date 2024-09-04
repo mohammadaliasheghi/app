e8fdccd91d51c96576794d55925aa875f0e46cd2
dev <mohammad.ali.asheghi@gmail.com>
2024-09-04 20:37:26 +0330

    change kafka producer message impl

--- oauth/src/main/java/com/mmad/oauth/util/MessageProducer.java
+++ oauth/src/main/java/com/mmad/oauth/util/MessageProducer.java
@@ -1,11 +1,18 @@
 package com.mmad.oauth.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageProducer.class);
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
@@ -14,7 +21,12 @@ public class MessageProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+    public void sendMessage(String topic, String data) {
+        LOGGER.info("Sending Message {}", data);
+        Message<?> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build();
+        kafkaTemplate.send(message);
     }
 }
